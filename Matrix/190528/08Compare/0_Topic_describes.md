0b33aae720d12be3a17dfcb27f8abbaed37a9320
hing1 <hing123@126.com>
2019-05-28 14:55:22 +0800

    uplate print stack

---
+++ Matrix/190528/08Compare/0_Topic_describes.md
@@ -0,0 +1,15 @@
+# Compare (eden)
+
+## Descrption
+
+Haoran is a "huge god" who loves C++ so much.One day he wants to implement a template function cmp which compares any type of two elements.</br>
+Now he passes the mission to you all and hopes you can implement it and makes it suitable to compare any two int, double, float, char, string and any two same type of pointers.</br>
+If the first parameter is equal to the second one, then return true, elsewise, false.</br>
+
+You can be careful that:
+
+1. When comparing two int, double, float, char*, string , you should compare their values.
+2. When comparing two pointers, you should compare the values they point to.
+3. the cmp function should always return a boolean value.
+
+出题人：劳思
---
+++ Matrix/190528/08Compare/cmp.h
@@ -0,0 +1,56 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+bool my_equal(const int a, const int b)
+{
+    return a == b;
+}
+bool my_equal(const string a, const string b)
+{
+    return a == b;
+}
+bool my_equal(const float a, const float b)
+{
+    return a == b;
+}
+bool my_equal(const double a, const double b)
+{
+    return a == b;
+}
+bool my_equal(const string *a, const string *b)
+{
+    return *a == *b;
+}
+bool my_equal(const char *s1, const char *s2)
+{
+    if (strcmp(s1, s2) == 0)
+        return true;
+    else
+        return false;
+}
+bool my_equal(const int *a, const int *b)
+{
+    return *a == *b;
+}
+bool my_equal(const float *a, const float *b)
+{
+    return *a == *b;
+}
+bool my_equal(const double *a, const double *b)
+{
+    return *a == *b;
+}
+
+template <class T>
+bool cmp(const T a, const T b)
+{
+    return my_equal(a, b);
+}
+
+#endif // SOURCE_H
