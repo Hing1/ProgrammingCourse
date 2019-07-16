5bca4c3d4995768f1927f6c84aa5cbca547ed638
hing1 <hing123@126.com>
2019-07-16 14:14:47 +0800

    create helloWorld and type conver

---
+++ Java/02data_type_conversion.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author Hing
+ *
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		a = a + 2;
+		System.out.println(a);
+		
+		int x = 10;
+		byte y = 5;
+		x = x + y;
+		System.out.println(x);
+		
+		byte m = 3;
+		m = (byte)(m + 5);
+		System.out.println(m);
+		
+		char ch = 'a';
+		System.out.println(ch + 1);
+		
+		System.out.println("a" + 1);
+		System.out.println("a" + 1 + 1);
+		System.out.println("a" + (1 + 1));
+		
+		byte b1 = 3, b2 = 4, b;
+//		b = b1 + b2; ±àÒë³ö´í
+		b = 3 + 4;
+	}
+}
