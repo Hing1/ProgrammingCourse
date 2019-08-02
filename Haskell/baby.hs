4d9c415db049ca19f0b0ce2563422787e6aecaeb
hing1 <hing123@126.com>
2019-08-02 17:46:57 +0800

    learn Haskell to the first 25 pages

---
+++ Haskell/baby.hs
@@ -0,0 +1,27 @@
+doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+
+doubleSmallNumber x = if x > 100 
+                  then x else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+circumference' :: Double -> Double 
+circumference' r = 2 * pi * r
+
+
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
