cdf88f79b3c925e0159aba3e2ddebbef890ecbbf
hing1 <hing123@126.com>
2019-09-03 13:55:51 +0800

    The assignment of struct

---
+++ Mr.Wang/C/47structArray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//结构体支持赋值
+
+typedef struct structArray
+{
+    int a[10];
+}Arr ;
+
+
+int main () {
+    Arr arr1 = {{1,2,3,4,5,6,7,8,9,10}};
+    Arr arr2;
+    arr2 = arr1;
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ",arr2.a[i]);
+    }
+    return 0;
+}
