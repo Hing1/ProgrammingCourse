52269d2844cc16a66ad061e112b5936c8bdca032
hing1 <hing123@126.com>
2019-09-04 08:10:01 +0800

    Add global variable version stack

--- Mr.Wang/C/52stack.c
+++ Mr.Wang/C/52stack.c
@@ -36,3 +36,43 @@ void push(MyStack * ps, char ch) {
 char pop(MyStack * ps) {
     return ps->mem[--ps->top];
 }
+
+#if 0
+#include <stdio.h>
+
+#define MAX 512
+
+//The global variable version
+
+struct MyStack {
+    char space[MAX];
+    int top;
+};
+
+struct MyStack myStack = {{0}, 0};
+
+int isEmpty() {
+    return myStack.top == 0;
+}
+int isFull() {
+    return myStack.top == MAX;
+}
+void push(char ch) {
+    myStack.space[myStack.top++] = ch;
+}
+char pop() {
+    return myStack.space[--myStack.top];
+}
+
+int main() {
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+        if(!isFull())
+            push(ch);
+    }
+    while (!isEmpty()) {
+        putchar(pop());
+    }
+    return 0;
+}
+
+#endif
