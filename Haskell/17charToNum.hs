3b98a3ba0a156e0bc861664987c7703a11b1d8ce
hing1 <hing123@126.com>
2019-09-25 17:20:13 +0800

    Realize transform char to numb

---
+++ Haskell/17charToNum.hs
@@ -0,0 +1,13 @@
+charToNum :: Char -> Int
+charToNum '0' = 0
+charToNum '1' = 1
+charToNum '2' = 2
+charToNum '3' = 3
+charToNum '4' = 4
+charToNum '5' = 5
+charToNum '6' = 6
+charToNum '7' = 7
+charToNum '8' = 8
+charToNum '9' = 9
+charToNum n = -1
+
