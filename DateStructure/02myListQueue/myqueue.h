8b65357749565ddee77502f00cb04a151df976bb
hing1 <hing123@126.com>
2019-09-26 13:00:39 +0800

    Move text in cpp file to head file.

--- DateStructure/02myListQueue/myqueue.h
+++ DateStructure/02myListQueue/myqueue.h
@@ -14,4 +14,28 @@ private:
 	MySingleList<Type> list;
 };
 
+template <typename Type>
+bool MyQueue<Type>::empty() const {
+    return list.empty();
+}
+
+template <typename Type>
+void MyQueue<Type>::push(Type const & obj) {
+    list.pushBack(obj);
+}
+
+template <typename Type>
+Type MyQueue<Type>::front() const {
+    if(empty())
+        throw underflow();
+    return list.front();
+}
+
+template <typename Type>
+Type MyQueue<Type>::pop() {
+    if(empty())
+        throw underflow();
+    return list.popFront();
+}
+
 #endif // MYQUEUE_H
--- DateStructure/02myListQueue/mysinglelist.h
+++ DateStructure/02myListQueue/mysinglelist.h
@@ -32,11 +32,156 @@ private:
     MySingleNode<Type> * _head;
 };
 
-class mallocError {
+class mallocError {};
+class underflow {};
 
-};
-class underflow {
+template <typename Type>
+MySingleList<Type> ::MySingleList() {
+    _head = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    if(nullptr == _head)
+        throw mallocError();
+    _head->next = nullptr;
+}
 
-};
+template <typename Type>
+MySingleList<Type> ::~MySingleList() {
+    while (_head) {
+        MySingleNode<Type> * temp = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+        temp = _head;
+        _head = _head->next;
+        free(temp);
+    }
+}
+
+template <typename Type>
+int MySingleList<Type> ::size() const{
+    if(empty())
+        return 0;
+
+    int num = 0;
+    MySingleNode<Type> * myHead = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    myHead = _head;
+    while (myHead) {
+        myHead = myHead->next;
+        num++;
+    }
+    return num;
+}
+
+template <typename Type>
+bool MySingleList<Type> ::empty() const{
+    return !(_head->next);
+}
+
+template <typename Type>
+Type MySingleList<Type> ::front() const{
+    if(empty())
+        throw underflow();
+    return _head->next->data;
+}
+
+template <typename Type>
+Type MySingleList<Type> ::back() const{
+    if(empty())
+        throw underflow();
+
+    MySingleNode<Type> * myHead = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    myHead = _head;
+    while (!myHead->next)
+        myHead = myHead->next;
+    return myHead->data;
+}
+
+template <typename Type>
+MySingleNode<Type> * MySingleList<Type> ::head() const{
+    if(empty())
+        throw underflow();
+    return _head;
+}
+
+template <typename Type>
+MySingleNode<Type> * MySingleList<Type> ::tail() const{
+    if(empty())
+        throw underflow();
+
+    MySingleNode<Type> * myHead = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    myHead = _head;
+    while (!myHead->next)
+        myHead = myHead->next;
+    return myHead;
+}
+
+template <typename Type>
+int MySingleList<Type> ::count(Type const & key) const{
+    if(empty())
+        throw underflow();
+
+    MySingleNode<Type> * myHead = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    myHead = _head;
+    int cou = 0;
+    while (myHead->next) {
+        cou++;
+        myHead = myHead->next;
+        if(myHead->data == key)
+            return cou;
+    }
+    return -1;
+}
+
+template <typename Type>
+void MySingleList<Type> ::pushFront(Type const & newData) {
+    MySingleNode<Type> * temp = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    temp->data = newData;
+    temp->next = _head->next;
+    _head->next = temp;
+}
+
+template <typename Type>
+void MySingleList<Type> ::pushBack(Type const & newData) {
+    MySingleNode<Type> * temp = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    MySingleNode<Type> * myHead = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    myHead = _head;
+
+    temp->data = newData;
+    while (myHead->next)
+        myHead = myHead->next;
+    temp->next = myHead->next;
+    myHead->next = temp;
+}
+
+template <typename Type>
+Type MySingleList<Type> ::popFront() {
+    if(empty())
+        throw underflow();
+    Type temp = _head->next->data;
+    _head = _head->next;
+    return temp;
+}
+
+template <typename Type>
+int MySingleList<Type> ::erase(Type const & key) {
+    if(empty())
+        throw underflow();
+
+    int cou = 1;
+    MySingleNode<Type> * myHead = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+    myHead = _head;
+    if(myHead->next->data == key) {
+        popFront();
+        return cou;
+    }
+    while (myHead->next->next) {
+        cou++;
+        myHead = myHead->next;
+        if(myHead->next->data == key) {
+            MySingleNode<Type> * temp = (MySingleNode<Type>*)malloc(sizeof(MySingleNode<Type>));
+            temp = myHead->next;
+            myHead->next = temp->next;
+            free(myHead);
+            return cou;
+        }
+    }
+    return -1;
+}
 
 #endif // MYSINGLELIST_H
