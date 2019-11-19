05ccdbf3ae99c6e85910feeb473568cac6db46ef
hing1 <hing123@126.com>
2019-11-19 22:59:57 +0800

    The takeWhile function

---
+++ Haskell/src/52-take-while.hs
@@ -0,0 +1,14 @@
+isAlpha :: Char -> Bool
+isAlpha x = elem x "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+
+getWords :: String -> String
+getWords [] = []
+getWords (x:xs)
+  | isAlpha x = x : (getWords xs)
+  | otherwise = []
+
+mytakeWhile :: (a -> Bool) -> [a] -> [a]
+mytakeWhile p [] = []
+mytakeWhile p (x:xs)
+  |p x = x : (mytakeWhile p xs)
+  |otherwise = []
